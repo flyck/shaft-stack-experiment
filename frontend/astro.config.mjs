412b0b231bed9b36a9d7d6e723f3cb33f5beabab
flyck <felix.brilej@googlemail.com>
2024-02-19 22:17:20 +0100

    feat(frontend): add basic graphql fetch and display

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import htmx from 'astro-htmx';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), htmx()],
+  integrations: [tailwind()],
 });
